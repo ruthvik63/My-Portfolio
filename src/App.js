7e588c3b3f03edc480e655991639dea2ec3c084c
ruthvik63 <ruthvikjadhav63@gmail.com>
2024-04-07 17:46:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import {darkTheme} from '../src/utils/themes'
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './components/Footer';
+
+const Body=styled.div`
+  background-color:${({theme})=>theme.bg};
+  width:100%;
+  height:100%
+  overflow-x:hidden;
+`
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+      <Navbar/>
+      <Body>
+        <Hero/>
+        <Wrapper>
+          <Skills/>
+          <Experience/>
+        </Wrapper>
+        <Education/>
+        <Footer/>
+      </Body>
+      </Router>
+      
+    </ThemeProvider>
+  );
+}
+
+export default App;
